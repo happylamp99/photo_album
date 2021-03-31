67dedea291916538dc10a1a4210480bacf4ddfe0
Tzekos <panagiotistzekos@hotmail.com>
2021-03-31 16:07:01 +0300

    Commit #1

--- src/main/java/ict/methodologies/Photos/controllers/AlbumController.java
+++ src/main/java/ict/methodologies/Photos/controllers/AlbumController.java
@@ -1,4 +1,35 @@
 package ict.methodologies.Photos.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+
 public class AlbumController {
+
+    @FXML
+    private ImageView imageView;
+
+    @FXML
+    private TextField textField1;
+
+    public void onMouseClick(MouseEvent mouseEvent) {
+        Button button = (Button) mouseEvent.getSource();
+        String buttonText = button.getText();
+
+        switch(buttonText){
+            case("Choose Image"):
+                Image image = new Image("E:/Tzekos/IdeaProjects/photo_album/images/pc1.jpg");
+                imageView.setImage(image);
+
+                break;
+            case("Clear"):
+                textField1.setText(" ");
+                break;
+
+        }
+    }
 }
