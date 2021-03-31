337ec3ba5b7f6d309a225b8dc391280c4ccfc78b
Tzekos <panagiotistzekos@hotmail.com>
2021-03-31 20:29:34 +0300

    Commit #1

--- src/main/java/ict/methodologies/Photos/controllers/AlbumController.java
+++ src/main/java/ict/methodologies/Photos/controllers/AlbumController.java
@@ -1,15 +1,14 @@
 package ict.methodologies.Photos.controllers;
 
+
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.FileChooser;
+import java.io.*;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
 
 
 public class AlbumController {
@@ -20,22 +19,28 @@ public class AlbumController {
     @FXML
     private TextField textField1;
 
-    public void onMouseClick(MouseEvent mouseEvent) throws FileNotFoundException {
+    String location = "E:/Tzekos/IdeaProjects/photo_album/savedImages";
+    public void onMouseClick(MouseEvent mouseEvent) throws IOException {
         Button button = (Button) mouseEvent.getSource();
         String buttonText = button.getText();
 
         switch(buttonText){
             case("Choose Image"):
-                    InputStream stream = new FileInputStream("E:/Tzekos/IdeaProjects/photo_album/images/pc1.jpg");
-                    Image image = new Image(stream);
-                    imageView.setImage(image);
-
+                FileChooser chooser = new FileChooser();
+                chooser.setTitle("Select Image File");
+                chooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.jpeg"));
+                File file = chooser.showOpenDialog(null);
+                Image image1 = new Image(file.toURI().toString());
+                imageView.setImage(image1);
                 break;
+
+            case("Insert"):
+
+
 
             case("Clear"):
                 textField1.setText(" ");
                 break;
-
         }
     }
 }
