07f2ecccc23b024c7b30893e6ea8bda0327079b9
kostas <gerogianniskostas@gmail.com>
2021-04-06 12:53:54 +0300

    back και clear στο ShowImages

--- src/main/java/ict/methodologies/Photos/controllers/ShowImagesController.java
+++ src/main/java/ict/methodologies/Photos/controllers/ShowImagesController.java
@@ -2,30 +2,61 @@ package ict.methodologies.Photos.controllers;
 
 import com.drew.imaging.ImageMetadataReader;
 import com.drew.imaging.ImageProcessingException;
+import ict.methodologies.Photos.PhotosApplication;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import com.drew.metadata.*;
 import com.drew.metadata.Directory;
+import javafx.scene.input.MouseEvent;
+
 import java.io.File;
 import java.io.IOException;
 
-public class ShowImagesController {
-//    @FXML
-//    private ImageView imageView2;
-//    File jpegFile = new File("src/main/resources/images/pic.png");
-//    Metadata metadata = ImageMetadataReader.readMetadata(jpegFile);
-//    for (Directory directory : metadata.getDirectories()) {
-//        for (Tag tag : directory.getTags()) {
-//            System.out.format("[%s] - %s = %s",
-//                    directory.getName(), tag.getTagName(), tag.getDescription());
-//        }
-//        if (directory.hasErrors()) {
-//            for (String error : directory.getErrors()) {
-//                System.err.format("ERROR: %s", error);
-//            }
-//        }
-//    }
 
-    public ShowImagesController() throws ImageProcessingException, IOException {
+public class ShowImagesController {
+    @FXML
+    private TextField textField1;
+
+    @FXML
+    private TextField textField2;
+
+    @FXML
+    private TextField textField3;
+
+    public void onMouseClick(MouseEvent mouseEvent) throws IOException {
+        Button button = (Button) mouseEvent.getSource();
+        String buttonText = button.getText();
+
+
+        switch (buttonText) {
+            case("Clear"):
+                textField1.setText(" ");
+                textField2.setText(" ");
+                textField3.setText(" ");
+                break;
+
+
+            case ("Back"):
+                try {
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource("/Menu.fxml"));
+                    Parent root = loader.load();
+
+                    PhotosApplication.getShowImagesStage().setScene(new Scene(root));
+                    PhotosApplication.getShowImagesStage().show();
+                } catch (IOException ex) {
+                    System.out.println(ex);
+                    break;
+
+
+                }
+        }
     }
 }
+
+
+
