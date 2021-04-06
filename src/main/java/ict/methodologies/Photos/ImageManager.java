d3a2d6639b9fb926daa2b6ed2748b85cef660d53
Tzekos <panagiotistzekos@hotmail.com>
2021-04-06 21:28:24 +0300

    changeIName method added, deleteImage method added

--- src/main/java/ict/methodologies/Photos/ImageManager.java
+++ src/main/java/ict/methodologies/Photos/ImageManager.java
@@ -7,21 +7,24 @@ public class ImageManager {
     private static EntityManagerFactory ENTITY_MANAGER_FACTORY = Persistence
             .createEntityManagerFactory("photo_album");
 
-    public static void main(String[] args){
-        addImage(1,"Image","Category");
+    public static void main(String[] args) {
+        addImage(2, "Image", "Category");
 
         ENTITY_MANAGER_FACTORY.close();
     }
+
     public static void addImage(int id, String name, String category) {
         EntityManager em = ENTITY_MANAGER_FACTORY.createEntityManager();
         EntityTransaction et = null;
         try {
             et = em.getTransaction();
             et.begin();
+
             Image image = new Image();
             image.setId(id);
             image.setiName(name);
             image.setiCategory(category);
+
             em.persist(image);
             et.commit();
         } catch (Exception ex) {
@@ -67,4 +70,49 @@ public class ImageManager {
         }
     }
 
+    public static void changeIName(int id, String name) {
+        EntityManager em = ENTITY_MANAGER_FACTORY.createEntityManager();
+        EntityTransaction et = null;
+        Image image = null;
+        try {
+            et = em.getTransaction();
+            et.begin();
+
+            image = em.find(Image.class, id);
+            image.setiName(name);
+
+            em.persist(image);
+            et.commit();
+
+        } catch (Exception ex) {
+            if (et != null) {
+                et.rollback();
+            }
+            ex.printStackTrace();
+        } finally {
+            em.close();
+        }
+    }
+
+    public static void deleteImage(int id) {
+        EntityManager em = ENTITY_MANAGER_FACTORY.createEntityManager();
+        EntityTransaction et = null;
+        Image image = null;
+        try {
+            et = em.getTransaction();
+            et.begin();
+            image = em.find(Image.class, id);
+            em.remove(image);
+
+            em.persist(image);
+            et.commit();
+        } catch (Exception ex) {
+            if (et != null) {
+                et.rollback();
+            }
+            ex.printStackTrace();
+        } finally {
+            em.close();
+        }
+    }
 }
